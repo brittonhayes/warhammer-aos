60ec2bc66826d02d7c35931193ebac5303f71e8b
Britton Hayes <brittonhayes@users.noreply.github.com>
2021-02-18 18:48:11 -0800

    adding first wave of data

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"embed"
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"os"
+	"text/template"
+)
+
+//go:embed data/json/*.json
+var f embed.FS
+
+//go:embed templates/json.tmpl
+var jsonTemplate string
+
+type Section struct {
+	Faction string
+	Items   []Mini
+}
+
+type Mini struct {
+	Name string
+	Size string
+}
+
+func main() {
+	genJSON()
+	// genCSV()
+	fmt.Println(r)
+}
+
+func genJSON() {
+	folder, err := f.ReadDir("data/json")
+	if err != nil {
+		panic(err)
+	}
+
+	var sections []Section
+	for i, file := range folder {
+		b, err := f.ReadFile("data/json/" + file.Name())
+		if err != nil {
+			panic(err)
+		}
+
+		var s Section
+		err = json.Unmarshal(b, &s)
+		if err != nil {
+			panic(err)
+		}
+
+		if i < 3 {
+			sections = append(sections, s)
+		}
+	}
+
+	tpl, _ := template.New("json").Parse(jsonTemplate)
+	if err := tpl.Execute(os.Stdout, sections); err != nil {
+		panic(err)
+	}
+}
