cee547f15276e96040985779ff61187cbf2256f9
Britton Hayes <brittonhayes@users.noreply.github.com>
2021-02-22 21:34:14 -0800

    Re-organized armies package out of internal

---
+++ lambda/armies/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	fiberadapter "github.com/awslabs/aws-lambda-go-api-proxy/fiber"
+	"github.com/brittonhayes/warhammer-aos/armies"
+	"github.com/brittonhayes/warhammer-aos/internal/handlers"
+)
+
+var fiberLambda *fiberadapter.FiberLambda
+
+func init() {
+	fiberLambda = handlers.New().AddRoute("*", armies.Handler()).Build()
+}
+
+func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return fiberLambda.ProxyWithContext(ctx, req)
+}
+
+func main() {
+	lambda.Start(handler)
+}
