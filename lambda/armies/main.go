42a6dd4319daec19271dd569052b119b602efbb2
Britton Hayes <brittonhayes@users.noreply.github.com>
2021-02-23 22:35:47 -0800

    implemented ability to query for different armies

--- lambda/armies/main.go
+++ lambda/armies/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -13,10 +14,14 @@ import (
 var fiberLambda *fiberadapter.FiberLambda
 
 func init() {
-	fiberLambda = handlers.New().AddRoute("*", armies.Handler()).Build()
+	fiberLambda = handlers.New().
+		AddRoute("*/:name", armies.Find()).
+		AddRoute("*/", armies.List()).
+		Build()
 }
 
 func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println(ctx.Value("Root"), req)
 	return fiberLambda.ProxyWithContext(ctx, req)
 }
 
